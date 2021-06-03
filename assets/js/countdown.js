c48b1cad6d4564ad73ffeb7f176fa529f8accb78
Nonprawich I <npcentreth@gmail.com>
2021-06-03 14:53:36 +0700

    Add Countdown

---
+++ assets/js/countdown.js
@@ -0,0 +1,10 @@
+var seconds_left = 6;
+var interval = setInterval(function() {
+    document.getElementById('timer_div').innerHTML = --seconds_left;
+
+    if (seconds_left <= 0)
+    {
+       document.getElementById('timer_div').innerHTML = "now!";
+       clearInterval(interval);
+    }
+}, 1000);
