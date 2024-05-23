bfc692c373b8174a97eb9922925ccbb41f458f49
Nonprawich I <npcentreth@gmail.com>
2024-05-23 15:32:40 +0700

    Change name

---
+++ Test2.py
@@ -0,0 +1,9 @@
+a=input("Enter the first number: ")
+b=input("Enter the second number: ")
+
+## x,y = input("Enter two number").split(",")
+
+a=int(a)
+b=int(b)
+c=a+b
+print("The summation of a and b is", c)
