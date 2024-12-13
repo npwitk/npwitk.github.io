c711f2792f7e02219b6807f84844fca09ddfa29e
Nonprawich I. <40120130+npwitk@users.noreply.github.com>
2024-12-13 13:49:38 +0700

    Update Personal Website V.2

--- Test2.py
+++
@@ -1,9 +0,0 @@
-a=input("Enter the first number: ")
-b=input("Enter the second number: ")
-
-## x,y = input("Enter two number").split(",")
-
-a=int(a)
-b=int(b)
-c=a+b
-print("The summation of a and b is", c)
